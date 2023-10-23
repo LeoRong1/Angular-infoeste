3a38742fbb45fcceb0291d011c30a2e1c2112753
Leo Rong <112832113+LeoRong1@users.noreply.github.com>
2023-10-23 09:58:15 -0300

    infoeste 2.0

---
+++ src/firebase-config.ts
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDFZn6INHdVl-k2OkJA6lsfHgmks1zhz7I",
+    authDomain: "leo-rong.firebaseapp.com",
+    projectId: "leo-rong",
+    storageBucket: "leo-rong.appspot.com",
+    messagingSenderId: "40375364083",
+    appId: "1:40375364083:web:93e3a66f76eed74cee4a86"
+};
